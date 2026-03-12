c0391e6c102bc008b379515e54d8c5031cab6789
syed shah <Shahshahbaz64@gmail.com>
2026-03-12 21:50:17 +0500

    automation backup

--- tests/specs/login.e2e.ts
+++ tests/specs/login.e2e.ts
@@ -1,9 +1,7 @@
 import LoginPage from '../pages/login.page'
-describe('Login flow1', () => {
+describe('Login flow', () => {
     it('should open the app', async () => {
-        // await driver.pause(3000)
         await LoginPage.waitUntilVisibleWithRetry(LoginPage.loginButton)
-        // console.log("login button BECOME VISIBLE")
         await LoginPage.pause(5000)
         await expect(await LoginPage.isLoginButtonDisplayed()).toBe(true)
         await LoginPage.getElement(LoginPage.loginButton)
